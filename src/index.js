1cdd6ecbc61162ac354df433c3f3c5f535361180
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-01-19 23:00:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let numArr = []
+    for (let i = 0; i < expr.length; i = i + 10) {
+        numArr.push(expr.slice(i, i + 10))
+    }
+
+    let morseArr = []
+    numArr.forEach((el) => {
+        let letter = ''
+        for (let j = 10; j > 0; j = j - 2) {
+            const elemStep = el[j - 2] + el[j - 1]
+            switch (elemStep) {
+                case '10':
+                    letter = '.' + letter
+                    break
+                case '11':
+                    letter = '-' + letter
+                    break
+                case '**********':
+                    letter = '' + letter
+                    break
+            }
+        }
+        morseArr.push(letter)
+    })
+
+    let result = ''
+    morseArr.forEach((item) => {
+        if (!MORSE_TABLE[item]) {
+            result = result + ' '
+        }
+        Object.keys(MORSE_TABLE).forEach((key) => {
+            if (item === key) {
+                result = result + MORSE_TABLE[key]
+            }
+        })
+    })
+
+    return result
 }
 
 module.exports = {
